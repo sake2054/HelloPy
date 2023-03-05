c209038e3be0d17442ce593185df7b089a632509
Sake <37127420+sake2054@users.noreply.github.com>
2023-03-05 22:24:30 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+class Main
+{
+public static void main(String args[]) throws Exception
+{
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+}
+}
